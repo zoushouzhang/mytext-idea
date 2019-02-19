35f29be83ae6c985dbe5e732d7e7b15a7ae4ccfc
zoushouzhang <1048325450@qq.com>
2019-02-19 21:23:22 +0800

    新建User.java,提交到本地仓库

---
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * 工作空间:LiuYang01
+ * 包名:com.itheima.domain
+ * 作者:zoushouzhang
+ * 日期:2019/2/19 21:12
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
